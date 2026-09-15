8e9f6f5ae8ae0d429cc05ea7bca0c5e5a3c6374a
thoriqwildan <wildanthoriq@gmail.com>
2026-09-15 21:11:26 +0700

    try: coba pakai controller

--- ps4controller-manager.ino
+++ ps4controller-manager.ino
@@ -1,5 +1,19 @@
 void notify() {
-  
+  if (PS4.LStickY() >= 126) {
+    leftForward();
+  } else if (PS4.LStickY() <= -126) {
+    leftBackward();
+  } else {
+    leftStop();
+  }
+
+  if (PS4.RStickY() >= 126) {
+    rightForward();
+  } else if (PS4.RStickY() <= -126) {
+    rightBackward();
+  } else {
+    rightStop();
+  }
 }
 
 void onConnect() {
